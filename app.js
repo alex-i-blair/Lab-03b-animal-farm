d9a4e4523084da7bb25ebac15c644542cb389894
Alex Blair <bclan11@gmail.com>
2021-10-06 15:11:41 -0700

    semi col

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // import functions
 
 // reference needed DOM elements
-const catImage = document.getElementById('cat-image')
-const catSound = document.getElementById('cat-sound')
-const dogImage = document.getElementById('dog-image')
-const dogSound = document.getElementById('dog-sound')
-const horseImage = document.getElementById('horse-image')
-const horseSound = document.getElementById('horse-sound')
+const catImage = document.getElementById('cat-image');
+const catSound = document.getElementById('cat-sound');
+const dogImage = document.getElementById('dog-image');
+const dogSound = document.getElementById('dog-sound');
+const horseImage = document.getElementById('horse-image');
+const horseSound = document.getElementById('horse-sound');
 
 catImage.addEventListener ('click', () => {
     catSound.play();
-})
+});
 dogImage.addEventListener ('click', () => {
     dogSound.play();
-})
+});
 horseImage.addEventListener ('click', () => {
     horseSound.play();
-})
+});
 
 // set event listeners 
     // get user input(s)
